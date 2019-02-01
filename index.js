d2ec168b511ca9b5f11f8cd01034a964979cc3bb
Antonina Maj <antoninajanina@gmail.com>
2019-02-01 18:56:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,10 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newKittens = slice
+  var newKittens = kittens.slice(0,kittens.length-1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice()
 }
